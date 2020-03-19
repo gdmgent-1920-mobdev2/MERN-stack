56a9bcb4cc9e74d9ae98e085fb2f0fa63feacdb4
drdynscript <drdynscript@gmail.com>
2020-03-19 16:03:50 +0100

    Added detail endpoint to solutionp

--- server/src/app/router/Router.ts
+++ server/src/app/router/Router.ts
@@ -1,14 +1,17 @@
 import { Application, NextFunction, Request, Response } from "express";
 
 import { FallbackController, HomeController } from '../controllers';
+import { default as ApiRouter } from '../api/router';
 
 class Router {
   private app: Application;
+  private apiRouter: ApiRouter;
   private homeController: HomeController;
   private fallbackController: FallbackController;
 
   constructor (app: Application) {
     this.app = app;
+    this.apiRouter = new ApiRouter();
 
     this.registerControllers();
     this.registerRoutes();
@@ -21,6 +24,7 @@ class Router {
 
   private registerRoutes () {
     this.app.route(['/', '/home']).all(this.homeController.index);
+    this.app.use('/api', this.apiRouter.router);
     this.app.use('/*', this.fallbackController.index);
   }
 }

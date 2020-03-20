52b9d178850e838a3b17bc275d3bb649d780a42f
drdynscript <drdynscript@gmail.com>
2020-03-20 13:05:36 +0100

    Added Mongoose models to the models folder and added seeding for this model via faker

--- server/src/app/services/database/MongoDBDatabase.ts
+++ server/src/app/services/database/MongoDBDatabase.ts
@@ -1,48 +1,92 @@
 import { default as mongoose, Connection } from 'mongoose';
+import { default as faker } from 'faker';
 
-import { ILogger } from "../logger";
-import { IConfig } from "../config";
+import { ILogger } from '../logger';
+import { IConfig } from '../config';
+import { IMessage, Message } from '../../models/mongoose';
 
 class MongoDBDatabase {
   private config: IConfig;
   private logger: ILogger;
   private db: Connection;
 
-  constructor (logger: ILogger, config: IConfig) {
+  constructor(logger: ILogger, config: IConfig) {
     this.logger = logger;
     this.config = config;
   }
 
-  public connect (): Promise<any> {
+  public connect(): Promise<any> {
     return new Promise<any>((resolve, reject) => {
-      mongoose.connect(this.config.mongoDBConnection, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      })
-      .then(data => {
-        this.db = mongoose.connection;
-        resolve(true);
-      })
-      .catch(error => {
-        this.logger.error('Can\'t connect to the database', error);
+      mongoose
+        .connect(this.config.mongoDBConnection, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        })
+        .then(data => {
+          this.db = mongoose.connection;
 
-        reject(error);
-      });
-    });    
+          this.logger.info('Connected to the mongodb database', {});
+
+          resolve(true);
+        })
+        .catch(error => {
+          this.logger.error('Can\'t connect to the database', error);
+
+          reject(error);
+        });
+    });
   }
 
-  public disconnect (): Promise<any> {
+  public disconnect(): Promise<any> {
     return new Promise<any>((resolve, reject) => {
-      this.db.close(true)
-      .then(data => {
-        resolve(data);
-      })
-      .catch(error => {
-        this.logger.error('Can\'t disconnect the database', error);
+      this.db
+        .close(true)
+        .then(data => {
+          resolve(data);
+        })
+        .catch(error => {
+          this.logger.error('Can\'t disconnect the database', error);
 
-        reject(error);
+          reject(error);
+        });
+    });
+  }
+
+  private messageCreate = async (body: string) => {
+    const message = new Message({ body });
+
+    try {
+      const newMessage = await message.save();
+
+      this.logger.info(`Message created with id ${newMessage._id}`, {});
+    } catch(error) {
+      this.logger.error('An error occurred when creating a message', error)
+    }
+  }
+
+  private createMessages = async () => {
+    await Promise.all([
+      (async () => this.messageCreate(faker.lorem.paragraph()))(),
+      (async () => this.messageCreate(faker.lorem.paragraph()))(),
+      (async () => this.messageCreate(faker.lorem.paragraph()))(),
+      (async () => this.messageCreate(faker.lorem.paragraph()))(),
+      (async () => this.messageCreate(faker.lorem.paragraph()))(),
+      (async () => this.messageCreate(faker.lorem.paragraph()))(),
+      (async () => this.messageCreate(faker.lorem.paragraph()))(),
+      (async () => this.messageCreate(faker.lorem.paragraph()))(),
+      (async () => this.messageCreate(faker.lorem.paragraph()))(),
+    ]);
+  }
+
+  public seed = async () => {
+    const messages = await Message.estimatedDocumentCount().exec()
+      .then(async count => {
+        if (count === 0) {
+          await this.createMessages();
+        }
+        return Message.find().exec();
       });
-    });    
+    console.log(messages);
   }
 }
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import { default as App } from './app';
 
-import { default as Config, IConfig } from './app/services/config';
+import { default as Config, IConfig, Environment } from './app/services/config';
 import Logger, { ILogger } from './app/services/logger';
 import MongoDBDatabase from './app/services/database';
 
@@ -11,27 +11,30 @@ import MongoDBDatabase from './app/services/database';
   // Create a Logger service
   const logger: ILogger = new Logger();
 
-  try {   
-
+  try {
     // Create a Database service
     const mongoDBDatabase = new MongoDBDatabase(logger, config);
     const connected = await mongoDBDatabase.connect();
+
+    if (config.env === Environment.development) {
+      mongoDBDatabase.seed();
+    }
 
     // Create the Express application
     const app: App = new App(logger, config);
     app.start();
 
+    // Stop all running processes
     const stopAllProcesses = async () => {
       app.stop();
       await mongoDBDatabase.disconnect();
 
-      console.log('Stopped all processes');
+      logger.info('Stopped all processes for this application', {});
     };
 
     process.on('SIGINT', () => stopAllProcesses());
     process.on('SIGTERM', () => stopAllProcesses());
-  } catch(error) {
-    logger.error('Can\'t launch the application', error);
+  } catch (error) {
+    logger.error("Can't launch the application", error);
   }
-  
 })(); // IIFE

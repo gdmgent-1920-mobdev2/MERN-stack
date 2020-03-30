e5bc69279c66c391a015a632fb217e7c9ecf3a7e
drdynscript <drdynscript@gmail.com>
2020-03-30 08:56:28 +0200

    Added navbar in navigation.js

--- react-client/src/app/components/header/Navigation.js
+++ react-client/src/app/components/header/Navigation.js
@@ -1,20 +1,20 @@
 import { default as React } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import * as Routes from '../../routes';
 
 const Navigation = ({children}) => {
   return (
-    <nav class="navbar navbar-expand-lg  navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        <img src="static/images/logo192.png" width="30" height="30" class="d-inline-block align-top" alt="" />
+    <nav className="navbar navbar-expand-lg  navbar-light bg-light">
+      <Link className="navbar-brand" to={Routes.LANDING}>
+        <img src="static/images/logo192.png" width="30" height="30" className="d-inline-block align-top" alt="" />
         Grafische en Digitale Media
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      </Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
           <NavLink className="nav-item nav-link" activeClassName="active" to={Routes.LANDING}>Home</NavLink>
           <NavLink className="nav-item nav-link" activeClassName="active" to={Routes.POSTS}>Nieuws</NavLink>
           <NavLink className="nav-item nav-link" activeClassName="active" to={Routes.CONTACT}>Contact</NavLink>
